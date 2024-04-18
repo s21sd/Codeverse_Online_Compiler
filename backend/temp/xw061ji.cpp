6951eab44ff378430b462d7446c0d18d32b9cff4
s21sd <srivastavasunny359@gmail.com>
2024-04-18 23:06:51 +0530

    dd

---
+++ backend/temp/xw061ji.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int day = 4;
+  switch (day) {
+  case 6:
+    cout << "Today is Saturday";
+    break;
+  case 7:
+    cout << "Today is Sunday";
+    break;
+  default:
+    cout << "Looking forward to the Weekend";
+}
+}
